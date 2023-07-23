bb149dd90b2317afd2df668fa701c804e018f6c8
Jeffrey Yeung <lattespirit@gmail.com>
2023-07-23 12:18:58 +0800

    Remove community packages

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,9 +3,4 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.colorscheme.tokyonight" },
 }
